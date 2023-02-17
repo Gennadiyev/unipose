5097a86c15136a93b3ff06d31b0009dde69eff98
xyrrrrrrrr <2841945284@qq.com>
2023-02-17 17:10:03 +0800

    [feat] Delete useless scripts from yolo_unipose

--- tests/test_yolo.py
+++
@@ -1,9 +0,0 @@
-from yolo_unipose.yolo_unipose import yolo_unipose
-
-def test_yolo():
-    model = yolo_unipose()
-    img = './tests/data/rxy.jpg'
-    bbox, class_name, confidence = model(img)
-    assert len(bbox) == 4
-    assert type(class_name) == str
-    assert confidence > 0.8
--- yolo_unipose/yolo_unipose.py
+++ yolo_unipose/yolo_unipose.py
@@ -17,12 +17,11 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import DetectMultiBackend
 from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
-from utils.general import (LOGGER, Profile, check_file, check_img_size, check_requirements, colorstr, cv2,
-                           increment_path, non_max_suppression, print_args, scale_boxes, scale_segments,
-                           strip_optimizer)
-from utils.plots import Annotator, colors, save_one_box
-from utils.segment.general import masks2segments, process_mask, process_mask_native
-from utils.torch_utils import select_device, smart_inference_mode
+from utils.general import (LOGGER, Profile, check_file, check_img_size, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, scale_boxes)
+from utils.plots import Annotator, colors
+from utils.segment.general import process_mask
+from utils.torch_utils import select_device
 
 
 class yolo_unipose(torch.nn.Module):
